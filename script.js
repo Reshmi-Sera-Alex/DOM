4bf737168531f83ff0cc26e3708eab066c46f922
Reshmi Sera Alex <reshmi.alex@gmail.com>
2024-09-30 13:46:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let q=document.getElementById("quote")
+q.innerHTML="Rise and Shine"
+
+let i=document.getElementsByClassName("info-box");
+i[0].style.backgroundColor="red"
+i[1].style.backgroundColor="yellow"
+i[2].style.backgroundColor="green"
+
+let s=document.getElementsByTagName("li");
+s[2].innerHTML="hello"
+
+let z=document.querySelector("footer span")
+x=z.innerHTML
+z.innerHTML=x+" Have a great day!"
